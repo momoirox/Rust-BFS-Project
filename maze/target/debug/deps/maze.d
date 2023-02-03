5038f51c8969bb37929ee0e8fc268dc27c92830c
Maja Blagic <maja.blagic@naovis.com>
2023-02-03 23:00:58 +0100

    Done

---
+++ maze/target/debug/deps/maze.d
@@ -0,0 +1,7 @@
+C:\Users\maja.blagic\Desktop\Rust-BFS-Project\maze\target\debug\deps\maze.exe: src\main.rs src\maze.rs src\state.rs
+
+C:\Users\maja.blagic\Desktop\Rust-BFS-Project\maze\target\debug\deps\maze.d: src\main.rs src\maze.rs src\state.rs
+
+src\main.rs:
+src\maze.rs:
+src\state.rs:
